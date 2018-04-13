c05b6b9cb2f1a5022844e5396eddddc745470e3e
Ben Miller <ben@benjaminmiller.com>
2018-04-13 18:52:49 -0400

    Forgot to add services and page_parser files

--- page_parser.py
+++ page_parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import services
+import json
 
 def get_coh_prices():
     coh_price_page = services.get_coh()
@@ -22,6 +23,99 @@ def get_coh_prices():
             item["quantity"] = cells[2].text.strip()
             item["price"] = cells[4].text.strip()[2:]
             items.append(item)
+    
+    return items
+        
+def get_ihav_prices():
+    ihav_price_page = services.get_ihav()
+    ihav_parsed = BeautifulSoup(ihav_price_page, "html.parser")
+    brand = ""
+    items = []
+    for row in ihav_parsed.select("td.body_boder > table > tr"):
+        inner_row = row.select("td > table > tr")[0]
+        cells = inner_row.select("td")
+        first_cell = cells[0]
+        cell_parts = first_cell.text.split(" - ")
+        
+        brand = cell_parts[0].strip()
+        name = cell_parts[1].strip()
+        quantity = cell_parts[2].strip()
+        
+        price_text = cells[2].text.strip()
+        price = price_text[3:]
+        
+        item = {}
+        item["brand"] = brand
+        item["name"] = name
+        item["quantity"] = quantity
+        item["price"] = price
+        items.append(item)
+    
+    return items
+
+def get_cubanlous_prices():
+    cl_price_page = services.get_cubanlous()
+    cl_json = json.loads(cl_price_page)
+    cl_parsed = BeautifulSoup(cl_json["productList"], "html.parser")
+    brand = ""
+    items = []
+    for row in cl_parsed.select(".product-container"):
+        product_name = row.select(".product-name")[0]
+        product_text = product_name.text
+        link = product_name.get("href")
+        
+        brand_begin = "https://www.cubanlous.com/"
+        brand_begin_idx = len(brand_begin)
+        
+        quantity_begin = "("
+        quantity_begin_idx = product_text.index(quantity_begin) + 1
+        quantity_end = ")"
+        quantity_end_idx = product_text.index(quantity_end)
+        quantity = product_text[quantity_begin_idx:quantity_end_idx]
+        
+        brand_end = "-cigars/"
+        name = ""
+        try:
+            brand_end_idx = link.index(brand_end)
+            brand = link[brand_begin_idx:brand_end_idx].replace("-", " ")
+            name = link[brand_end_idx+len(brand_end):].replace("-"+quantity, "").replace("-", " ")
+            name = name.replace(brand, "")
+        except:
+            continue
+        
+        price = row.select(".product-price")[0].text.strip()[1:]
+        
+        item = {}
+        item["brand"] = brand.strip().title()
+        item["name"] = name.strip().title()
+        item["quantity"] = quantity
+        item["price"] = price
+        items.append(item)
+    
+    for item in items:
+        print(item["brand"] + "|" + item["name"] + "|" + item["quantity"] + "|" + item["price"])
+    
+def get_finestcc_prices():
+    finestcc_price_page = services.get_finestcc()
+    finestcc_parsed = BeautifulSoup(finestcc_price_page, "html.parser")
+    brand = ""
+    items = []
+    for brand_section in finestcc_parsed.select("div.category_products_container"):
+        try:
+            brand = brand_section.select(".subcategory_title")[0].text.strip()
+        except:
+            continue
+        
+        for product in brand_section.select(".product_container"):
+            name = product.select(".product_title")[0].text
+            
+            for option in product.select(".product_price > div"):
+                item = {}
+                item["brand"] = brand
+                item["name"] = name.replace(brand, "").strip()
+                item["price"] = option.select(".original,.special")[0].text.strip()[3:]
+                item["quantity"] = option.select(".sticks")[0].text.strip()
+                items.append(item)
     
     for item in items:
         print(item["brand"] + "|" + item["name"] + "|" + item["quantity"] + "|" + item["price"])
--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 from requests import get,post,Session
 from requests.exceptions import RequestException
 from contextlib import closing
+import time
 
 def get_coh():
     s = Session()
@@ -17,6 +18,56 @@ def get_coh():
         log_error('Error during requests to {0} : {1}'.format(url, str(e)))
         return None
 
+def get_ihav():
+    s = Session()
+    s.get("https://www.ihavanas.com/")
+    url = "https://www.ihavanas.com/advancesearch.php"
+    headers = { "Content-Type": "multipart/form-data; boundary=---------------------------7845160828623" }
+    body = '-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtSearch"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtregemail"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtregpswd"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtrm"\r\n\r\nN\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="cmbtype"\r\n\r\n0\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="cmbbrand"\r\n\r\n0\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtproduct"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtPF"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtPT"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtLF"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtLT"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtRGF"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtRGT"\r\n\r\n\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="btnSubmit"\r\n\r\nSubmit\r\n-----------------------------7845160828623\r\nContent-Disposition: form-data; name="txtNL"\r\n\r\nN\r\n-----------------------------7845160828623--\r\n'
+    try:
+        with closing(s.post(url, data=body, headers=headers, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content.replace(b'54%"<', b'54%"><')
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+    
+def get_cubanlous():
+    s = Session()
+    s.get("https://www.cubanlous.com/")
+    timestamp = round(time.time() * 1000)
+    url = "https://www.cubanlous.com/modules/blocklayered_mod/blocklayered_mod-ajax.php?id_category_layered=81&layered_price_slider=49_950&orderby=name&orderway=asc&n=500forceSlide&_=" + str(timestamp)
+    headers = { "Accept": "application/json, text/javascript, */*; q=0.01" }
+    try:
+        with closing(s.get(url, headers=headers, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+    
+def get_finestcc():
+    s = Session()
+    s.get("https://www.finestcubancigars.com/")
+    timestamp = round(time.time() * 1000)
+    url = "https://www.finestcubancigars.com/brands/all-cigar-brands.html"
+    headers = { "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" }
+    try:
+        with closing(s.get(url, headers=headers, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
 
 def is_good_response(resp):
     """
